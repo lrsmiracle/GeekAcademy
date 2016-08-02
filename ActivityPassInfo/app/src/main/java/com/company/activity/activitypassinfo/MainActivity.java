e5a24256f01fe4709095e8360c9b998a4b86243a
lrsmiracle <lrsmiracle@163.com>
2016-08-02 22:17:15 +0800

    Start to learn Android
    Learn how to send message between two Activity

---
+++ ActivityPassInfo/app/src/main/java/com/company/activity/activitypassinfo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.company.activity.activitypassinfo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mEditText = null;
+    private Button mButton = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mEditText = (EditText) findViewById(R.id.editText_main);
+        mButton = (Button) findViewById(R.id.button_main);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                intent.putExtra("Message",mEditText.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+    }
+}
